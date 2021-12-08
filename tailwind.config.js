b42b2bee10ecc7931ab2720dfbfbd1594b181277
Mike <migueligr2000@gmail.com>
2021-12-08 17:58:33 +0100

    tailwind active

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  purge: {enabled:false, content: ['./build/**/*.php']},
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
