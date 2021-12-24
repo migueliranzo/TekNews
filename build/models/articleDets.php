a486712fe16919ec3043eff7f1a219e7fa057d42
Mike <migueligr2000@gmail.com>
2021-12-24 01:39:38 +0100

    switch edit view, only on lg

--- build/models/articleDets.php
+++ build/models/articleDets.php
@@ -38,27 +38,30 @@ $Parsedown = new Parsedown();
 ?>
 <script src="https://cdn.jsdelivr.net/remarkable/1.7.1/remarkable.min.js"></script>
 <script>
-  var display = true;
+  var display = false;
+  var orientation = true;
   var md = new Remarkable();
 
   function mrkToHtml() {
     document.getElementById("contentView").innerHTML = md.render(document.getElementById("contentEdit").value);
     let attribute =  document.getElementById("contentView").offsetHeight;
 
-    document.getElementById("contentEdit").style.height = attribute+20;
+  //  document.getElementById("contentEdit").style.height = attribute+20;
   }
 
   function enableEdit() {
 
     if (display == true) {
       document.getElementById("contentPanel").setAttribute("style", "display:none");
-      document.getElementById("switchView").setAttribute("style", "display:none")
+      document.getElementById("switchView").setAttribute("style", "display:none");
       document.getElementById("editBtn").innerHTML = "Edit";
+      
+    // document.getElementById("viewContainer").setAttribute("style", " align-items: center ");
       display = false;
     } else {
       document.getElementById("contentPanel").setAttribute("style", "display:block");
       document.getElementById("editBtn").innerHTML = "<i class='fas text-xl fa-times'></i>";
-      document.getElementById("switchView").setAttribute("style", "display:inline-block")
+      document.getElementById("switchView").setAttribute("style", "display:inline-block");
 
       display = true;
 
@@ -67,15 +70,31 @@ $Parsedown = new Parsedown();
 
   function switchView() {
 
+    if(orientation){
 
+      document.getElementById("mainContainer").style.flexDirection = "column";
+      document.getElementById("mainContainer").style.alignItems = "center";
+      orientation = false;
+    }else{
+      document.getElementById("mainContainer").style.flexDirection = "row";
+      document.getElementById("mainContainer").style.alignItems = "flex-start";
+      orientation = true;
+    }
+    
   }
 
+  function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight+10)+"px";
+}
 
 
 </script>
 
 <div class="text-right">
-  <button onclick="switchView()" style="display: none;" id="switchView" class="btn bg-pink-600 hover:bg-pink-300 text-white"><i class="fas text-lg fa-random"></i></button>
+  <div id="switchView" class="hidden">
+  <button onclick="switchView()"    class="btn bg-pink-600 hover:bg-pink-300 text-white hidden xl:block"><i class="fas text-lg fa-random"></i></button>
+  </div>
   <button onclick="enableEdit()" id="editBtn" class="btn bg-pink-600 hover:bg-pink-300 text-white">Edit</button>
 
 
@@ -84,18 +103,18 @@ $Parsedown = new Parsedown();
 
 
 
-<div class="flex  flex-col xl:flex-row  xl:items-start items-center mt-6  gap-8">
+<div id="mainContainer" class="flex  flex-col xl:flex-row  xl:items-start items-center mt-6  gap-8">
   <!-- Content -->
-  <div class="flex flex-1 flex-col">
-    <article id="contentView" class=" prose max-w-[75ch] text-justify break-all">
+  <div class="flex flex-1 flex-col items-center" id="viewContainer">
+    <article id="contentView" class=" prose max-w-[75ch] text-justify break-words">
       <?php echo $Parsedown->text($content); ?>
     </article>
 
   </div>
   <!-- IMG -->
-  <div id="contentPanel" class="flex flex-1  max-w-[75ch] w-[100%] flex-col">
+  <div id="contentPanel" class=" flex-1  max-w-[75ch] w-[100%] flex-col hidden">
     <form action="articleDets.php" class="w-[100%]">
-      <textarea id="contentEdit" onkeyup="mrkToHtml()" class="w-[100%] h-[500px] border border-black rounded p-2">
+      <textarea id="contentEdit" oninput="auto_grow(this)" onkeyup="mrkToHtml()" class="w-[100%] h-[500px] border border-black rounded p-2">
         <?php echo ($content); ?>
       </textarea>
       <br>
@@ -103,34 +122,13 @@ $Parsedown = new Parsedown();
     </form>
   </div>
 </div>
-
-<!-- 
-<div class="mx-auto place-content-center grid ">
-
-  <div class="">
-    <article id="contentView" class="prose max-w-[75ch]">
-      <?php echo $Parsedown->text($content); ?>
-    </article>
-  </div>
-
-  <div id="contentPanel" class="class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10 >
-    <form action="articleDets.php">
-
-      <textarea id="contentEdit" onkeyup="mrkToHtml()" class="  my-2 border-2 border-black rounded-2xl p-2 w-[100%] h-96" id="contentInput">
-<?php echo ($content); ?>
-</textarea>
-      <br>
-      <button class="btn bg-blue-500 text-white hover:bg-blue-300 " type="submit">Save Changes!</button>
-    </form>
-  </div>
--->
 </div>
 
 <script>
 
-let attribute =  document.getElementById("contentView").offsetHeight;
+let attribute =  document.getElementById("contentView").offsetHeight + 80;
+document.getElementById("contentEdit").style.height = attribute + "px";
 
-document.getElementById("contentEdit").style.height = attribute+20;
 
 console.log(attribute);
 
