c68ef30472e7ba9d07bb71acb23cef45bf7b677e
Mike <migueligr2000@gmail.com>
2022-01-18 20:08:49 +0100

    small menu tweaks

--- build/models/header.php
+++ build/models/header.php
@@ -118,11 +118,11 @@ if (session_status() != PHP_SESSION_ACTIVE) {
 				</div>
 		<div class="xl:hidden lg:hidden">
 	
-		<div id="sideMenu" class=" hidden bg-blue-500 border-l-2 border-b-2 rounded-bl-full text-white right-0 top-0 md:w-[45vw] sm:w-[52vw] w-[60vw]  overflow-hidden fixed z-[999]">
+		<div id="sideMenu" class=" hidden bg-neutral-700 border-l-2 border-b-2 rounded-bl-full text-white right-0 top-0 md:w-[45vw] sm:w-[52vw] w-[60vw]  overflow-hidden fixed z-[999]">
 	
 		<div> 
 				<ul>
-					<li class="pt-4  pl-4  transition text-xl"> <i class='hover:text-pink-500 hover:cursor-pointer pl-6 pr-1 fas fa-times'></i>
+					<li class="pt-4 bg-neutral-200 pl-4 text-black  transition flex text-xl pb-1"> <p class="text-left flex-1">Menu</p>  <i class='flex-1 pr-[30px] hover:text-pink-500 text-right hover:cursor-pointer pl-6 fas fa-times'></i>
 					</li>
 
 					<?php
@@ -130,32 +130,32 @@ if (session_status() != PHP_SESSION_ACTIVE) {
 					if (isset($_SESSION["name"]) && $_SESSION["role"] == 0) {
 
 						if (strpos($_SERVER['REQUEST_URI'], "userProfile.php") !== false) {
-							echo "<a href='services/logOff.php'> <li class='px-7 py-3 uppercase text-right sm:text-center  hover:bg-pink-500' transition> Log out <i class='pl-6 pr-1 fas fa-times'></i> </li></a>";
+							echo "<a href='services/logOff.php'> <li class='px-7 py-3 uppercase text-right sm:text-center  hover:text-pink-500' transition> Log out <i class='pl-6 pr-1 fas fa-times'></i> </li></a>";
 						} else {
-							echo "<a href='userProfile.php'> <li class='px-7 py-3 uppercase text-right sm:text-center  hover:bg-pink-500' transition> Profile <i class='pl-6  fas fa-user-circle'></i>  </li></a>";
-							echo "<a href='services/logOff.php'> <li class='px-7 py-3 uppercase text-right sm:text-center  hover:bg-pink-500' transition> Log out <i class='pl-6 pr-1 fas fa-times'></i> </li></a>";
+							echo "<a href='userProfile.php'> <li class='px-7 py-3 uppercase text-right sm:text-center  hover:text-pink-500' transition> Profile <i class='pl-6  fas fa-user-circle'></i>  </li></a>";
+							echo "<a href='services/logOff.php'> <li class='px-7 py-3 uppercase text-right sm:text-center  hover:text-pink-500' transition> Log out <i class='pl-6 pr-1 fas fa-times'></i> </li></a>";
 						}
 					} else if ((isset($_SESSION["name"]) && ($_SESSION["role"] == 1) || ($_SESSION["role"] == 2))) {
 
-						echo "<a href='services/logOff.php'> <li class='px-7 py-3 uppercase text-right sm:text-center  hover:bg-pink-500' transition> Log out <i class='pl-6 pr-1 fas fa-times'></i> </li></a>";
+						echo "<a href='services/logOff.php'> <li class='px-7 py-3 uppercase text-right sm:text-center  hover:text-pink-500' transition> Log out <i class='pl-6 pr-1 fas fa-times'></i> </li></a>";
 					} else {
-						echo "<a href='login.php'> <li class='px-7 py-3 uppercase text-right sm:text-center  hover:bg-pink-500' transition> Login <i class='pl-6  fas fa-user-circle'></i>  </li></a>";
+						echo "<a href='login.php'> <li class='px-7 py-3 uppercase text-right sm:text-center  hover:text-pink-500' transition> Login <i class='pl-6  fas fa-user-circle'></i>  </li></a>";
 					}
 					?>
 
 
 					<?php if ($_SESSION["role"] == 1 || $_SESSION["role"] == 2) {   ?> <a href='article.php?new=1'>
-							<li class='px-7 py-3 uppercase text-right sm:text-center  hover:bg-pink-500' transition> Create article <i class="pl-6 pr-1 fas fa-folder-plus"></i> </li>
+							<li class='px-7 py-3 uppercase text-right sm:text-center  hover:text-pink-500' transition> Create article <i class="pl-6 pr-1 fas fa-folder-plus"></i> </li>
 						</a> <?php } ?>
 					
 						<li class="md:hidden block">
 						<form class="mb-0 flex  flex-col" action="search.php" method="GET">
 							<ul class=" text-right sm:text-center  ">
-						<li class="border-y-2 pr-6 border-pink-500 mt-2"> <p class="py-2   uppercase">Categories <i class="pl-6 pr-2 inline fas fa-list"></i></p>  </li>
-						<li class="hover:bg-pink-500 py-1 hover:cursor-pointer"><button type="submit" value="1" name="type" class="py-3 px-6    text-white ">Crypto World</button></li>
-						<li class="hover:bg-pink-500 py-1 hover:cursor-pointer"><button type="submit" value="0" name="type" class="py-3 px-6    text-white ">Tech News</button></li>
-						<li class="hover:bg-pink-500 py-1 hover:cursor-pointer"><button type="submit" value="3" name="type" class="py-3 px-6   text-white ">Biotechnology</button></li>
-						<li class="hover:bg-pink-500 py-1 hover:cursor-pointer"><button type="submit" value="2" name="type" class=" py-3 px-6   text-white ">VR</button></li>
+						<li class="pr-6 bg-neutral-500 mt-2"> <p class="py-2   uppercase">Categories <i class="pl-6 pr-2 inline fas fa-list"></i></p>  </li>
+						<li class=" py-1"><button type="submit" value="1" name="type" class="py-3 px-6  hover:text-pink-500   text-white ">Crypto World</button></li>
+						<li class=" py-1"><button type="submit" value="0" name="type" class="py-3 px-6  hover:text-pink-500   text-white ">Tech News</button></li>
+						<li class=" py-1"><button type="submit" value="3" name="type" class="py-3 px-6  hover:text-pink-500  text-white ">Biotechnology</button></li>
+						<li class=" py-1"><button type="submit" value="2" name="type" class=" py-3 px-6 hover:text-pink-500   text-white ">VR</button></li>
 						<li class="sm:mb-10"></li>
 				</ul>
 				</form>
